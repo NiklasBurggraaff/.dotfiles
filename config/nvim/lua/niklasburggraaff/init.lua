c8b869374c6698bfb14cffe69baf0a553964c366
Niklas Burggraaff <niklasburggraaff@gmail.com>
2023-06-24 14:21:06 +0100

    Refactor nvim config

---
+++ config/nvim/lua/niklasburggraaff/init.lua
@@ -0,0 +1,10 @@
+require("niklasburggraaff.plugins-setup")
+require("niklasburggraaff.keymaps")
+require("niklasburggraaff.options")
+
+-- Plugins
+require("niklasburggraaff.plugins.misc")
+require("niklasburggraaff.plugins.cmp")
+require("niklasburggraaff.plugins.lsp")
+require("niklasburggraaff.plugins.telescope")
+require("niklasburggraaff.plugins.treesitter")
